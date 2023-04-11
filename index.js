877f725a8d0477ea518c1a41bcee0d259dfae4e3
nicopina <pinanicolasagustin@gmail.com>
2023-04-11 17:29:29 -0300

    nuevo pull

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ pm.addProduct({
     title: 'Cheesecake',
     description: 'Torta de queso con frutos rojos',
     price: 4000,
-    thumbnail: '',
+    thumbnail: 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F21%2F2015%2F02%2F11%2Fcheesecake-facil-con-leche-condensada-2000.jpg',
     code: 'PROD1',
     stock: 4
 })
@@ -43,7 +43,24 @@ pm.addProduct({
     title: 'Torta Oreo',
     description: 'Torta chocolate y mousse a base de galletas oreo',
     price: 5000,
-    thumbnail: '',
+    thumbnail: 'https://pasteleriadc.com/wp-content/uploads/torta-dripcake-helado-768x1078.jpg',
     code: 'PROD2',
-    stock: 4
+    stock: 2
 })
+pm.addProduct({
+    title: 'Lemon Pie',
+    description: 'Pastel de mousse de limon con merengue',
+    price: 4500,
+    thumbnail: 'https://img2.rtve.es/i/?w=1600&i=1635859279860.jpg',
+    code: 'PROD3',
+    stock: 3
+})
+
+const products = pm.getProducts()
+console.log(products)
+
+const product = pm.getProductsById(1)
+console.log(product)
+
+const nonExistentProduct = pm.getProductsById(100)
+console.log(nonExistentProduct)
